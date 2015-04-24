c768f020a3d0dfd5e35b1a2fd59ed5631cc8dc6c
CeShine Lee <shuanck@gmail.com>
2015-04-24 22:24:43 +1000

    Add Docker installtion script

--- setup.sh
+++ setup.sh
@@ -6,10 +6,13 @@ sudo apt-get update
 
 sudo apt-get install -y git-core byobu
 
-# Install emacs
+# Install Emacs
 sudo apt-get install -y emacs24
 
-# Install python
+# Install Python
 sudo apt-get install -y python-pip pypy 
 sudo pip install -U virtualenvwrapper
-mkdir ~/.virtualenvs
+mkdir -p ~/.virtualenvs
+
+# Install Docker
+sudo -i wget -qO- https://get.docker.com/ | sh
